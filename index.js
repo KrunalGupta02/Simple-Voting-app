5b326a6237036ea656e01895ec6864355851a952
Krunal Gupta <guptakrunal02@gmail.com>
2024-05-06 01:32:13 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const io = require("socket.io")(8000, { cors: { origin: "*" } });
+import { createServer } from "http";
+import { Server } from "socket.io";
 
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+httpServer.listen(3500, () => {
+  console.log("Listening on port 3500");
+});
 let totalVotes = 0;
 let votingPolls = {
   html: 0,
   css: 0,
   javascript: 0,
   react: 0,
-  java: 0,
+  python: 0,
 };
 
 io.on("connection", (socket) => {
