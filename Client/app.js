22a8d3b5a68eeb4130b770850d648d6da559b36f
Krunal Gupta <guptakrunal02@gmail.com>
2024-05-06 00:09:25 +0530

    make a simple programming app

---
+++ Client/app.js
@@ -0,0 +1,58 @@
+// Client side code
+const socket = io("ws://localhost:3500");
+
+const progressBoxes = document.querySelectorAll(".progress-box");
+const percentTags = document.querySelectorAll(".percent-tag");
+const totalVotesElem = document.getElementById("totalVotes");
+
+// Initially user not voted anyone
+let vote = false;
+
+for (let i = 0; i < progressBoxes.length; i++) {
+  const element = progressBoxes[i];
+  //   console.log(element);
+
+  element.addEventListener("click", () => {
+    addVote(element, element.id);
+  });
+}
+
+const addVote = (element, id) => {
+  // If vote is already done don't do anything (one voter only do one time voting)
+  if (vote) {
+    return;
+  }
+
+  let voteTo = id;
+  socket.emit("send-vote", voteTo); // Sending the msg for which client user votes
+  vote = true;
+  element.classList.add("active");
+
+  socket.on("receive-vote", (data) => {
+    updatePolls(data);
+  });
+
+  socket.on("update", (data) => {
+    updatePolls(data);
+  });
+};
+
+const updatePolls = (data) => {
+  let votingData = data.votingPolls;
+  console.log(votingData);
+  let totalVotes = data.totalVotes;
+
+  totalVotesElem.innerHTML += totalVotes;
+
+  // Update the progress bars for each voting option
+  percentTags.forEach((percentTag, index) => {
+    let vote = votingData[progressBoxes[index].id];
+
+    // Calculate the width based on vote
+    let width = Math.round((vote / totalVotes) * 100);
+
+    // If width is 0 set the data attr to 0 otherwise the width
+    percentTag.setAttribute("data", `${!width ? 0 : width}%`);
+    percentTag.style.width = `${!width ? 0 : width}%`;
+  });
+};
---
+++ index.js
@@ -0,0 +1,41 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+httpServer.listen(3500, () => {
+  console.log("Listening on port 3500");
+});
+
+let totalVotes = 0;
+let votingPolls = {
+  html: 0,
+  css: 0,
+  javascript: 0,
+  react: 0,
+  java: 0,
+};
+
+io.on("connection", (socket) => {
+  console.log(`User ${socket.id} connected`);
+
+  socket.emit("update", { votingPolls, totalVotes });
+
+  // receiving
+  socket.on("send-vote", (voteTo) => {
+    totalVotes += 1;
+    console.log(`${socket.id} is voted to ${voteTo}`);
+
+    // update the party vote
+    votingPolls[voteTo] += 1;
+
+    socket.broadcast.emit("receive-vote", { votingPolls, totalVotes });
+    socket.emit("update", { votingPolls, totalVotes });
+  });
+});
